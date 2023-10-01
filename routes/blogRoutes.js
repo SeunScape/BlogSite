6310268862435ed874b6975de879cb42c3c3d442
RIX <69199129+SeunScape@users.noreply.github.com>
2023-10-01 23:28:21 +0100

    fresh commit

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { homepage, addNewRecipe } from '../server/controllers/blogController.js';
+import { homepage, addNewRecipe, exploreCategories } from '../server/controllers/blogController.js';
 const router = express.Router();
 
 router.get('/', homepage);
 
+router.get('/categories', exploreCategories);
 router.route('/recipe')
 .post(addNewRecipe);
 
--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -3,21 +3,35 @@ import {RecipeSchema} from '../models/model.js'
 
 const Recipe = mongoose.model('Recipe', RecipeSchema);
 
-export const homepage = (req, res) => {
-  res.render('index.ejs', {title: 'Template blog- home'});
+export const homepage = async(req, res) => {
+  try {
+    const limitNumber = 3;
+    const categories = await Recipe.find({}).limit(limitNumber);
+    res.render('index.ejs', {title: 'Template blog- home', categories});
+  }catch(error){
+    res.status(500).send({message: error.message || "error occured"})
+  }
 };
 
-export const addNewRecipe = async(req, res) => {
-  const post = new Recipe({
-      name: req.body.name, 
-      image: req.body.image, 
-  }); 
+export const exploreCategories = async(req, res) => {
   try {
-      let data = await post.save()
-      res.status(200).json(data)
-  } catch (error) {
-      res.status(500).json({ message: error}); 
+    const limitNumber = 20;
+    const categories = await Recipe.find({}).limit(limitNumber);
+    res.render('categories.ejs', {title: 'Template blog- home', categories});
+  }catch(error){
+    res.status(500).send({message: error.message || "error occured"})
   }
 };
 
 
+export const addNewRecipe = async (req, res) => {
+  try {
+    const recipesData = req.body;
+
+    const data = await Recipe.insertMany(recipesData);
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
